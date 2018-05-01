a41ab67dc1bfced605b68e07f3dde4d4b17a8da6
Joseph Hutchinson <hutch7995@gmail.com>
2018-04-30 23:36:15 -0400

    menu overlay fade in

--- static/js/core.js
+++ static/js/core.js
@@ -1,8 +1,20 @@
 
+
 function cardPressed() {
     this.classList.add('card-hover');
 }
 
 function cardReleased() {
     this.classList.remove('card-hover');
+}
+
+function hamburgerMenuPressed() {
+    if (document.body.classList.contains('no-scroll')) {
+        document.body.classList.remove('no-scroll');
+        document.body.style.paddingRight = 0 + "px";
+    } else {
+        document.body.style.paddingRight = window.innerWidth - document.documentElement.clientWidth + "px";
+        document.body.classList.add('no-scroll');
+    }
+    
 }
