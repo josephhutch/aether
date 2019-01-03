26e4dbfa7a3838617c51aa5a756dc02039713ac4
Joe Hutchinson <HutchinsonJoseph@JohnDeere.com>
2019-01-03 10:40:57 -0500

    Make menu more accessible

--- static/js/core.js
+++ static/js/core.js
@@ -9,12 +9,16 @@ function cardReleased() {
 }
 
 function hamburgerMenuPressed() {
-    if (document.body.classList.contains('no-scroll')) {
+    if (this.parentNode.classList.contains('hamburger-menu-open')) {
         document.body.classList.remove('no-scroll');
+        this.parentNode.classList.remove('hamburger-menu-open')
+        this.setAttribute('aria-expanded', "false");
         document.body.style.paddingRight = 0 + "px";
     } else {
         document.body.style.paddingRight = window.innerWidth - document.documentElement.clientWidth + "px";
         document.body.classList.add('no-scroll');
+        this.parentNode.classList.add('hamburger-menu-open')
+        this.setAttribute('aria-expanded', "true");
     }
     
 }
