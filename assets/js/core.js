19f5687edf606879f72aaa7022dd289d42a9488a
Joe Hutchinson <hutch7995@gmail.com>
2022-03-03 18:36:20 -0500

    Make overlay menu work without js

--- assets/js/core.js
+++ assets/js/core.js
@@ -7,16 +7,14 @@ function cardReleased() {
 }
 
 function hamburgerMenuPressed() {
-    if (this.parentNode.classList.contains('hamburger-menu-open')) {
-        document.body.classList.remove('no-scroll');
-        this.parentNode.classList.remove('hamburger-menu-open')
-        this.setAttribute('aria-expanded', "false");
-        document.body.style.paddingRight = 0 + "px";
-    } else {
+    if (this.checked) {
+        this.setAttribute('aria-expanded', "true");
         document.body.style.paddingRight = window.innerWidth - document.documentElement.clientWidth + "px";
         document.body.classList.add('no-scroll');
-        this.parentNode.classList.add('hamburger-menu-open')
-        this.setAttribute('aria-expanded', "true");
+    } else {
+        this.setAttribute('aria-expanded', "false");
+        document.body.classList.remove('no-scroll');
+        document.body.style.paddingRight = 0 + "px";
     }
     
 }
